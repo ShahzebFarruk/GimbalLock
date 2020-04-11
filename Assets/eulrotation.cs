b8d628ed172612eb9fde7bcac45a9e0577eca37d
ShahzebFarruk <61950234+ShahzebFarruk@users.noreply.github.com>
2020-04-11 15:40:05 -0400

    The Project with the current scene is added for the Gimbal LOck problem code. Pl test it.

---
+++ Assets/eulrotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eulrotation : MonoBehaviour 
+{
+    public Vector3 eulerrot;
+    public Vector3 rotationAngles;
+
+    void Start () 
+    {
+         eulerrot = new Vector3 (0f, 90f, 0f);
+        this.transform.eulerAngles =  eulerrot;
+
+        rotationAngles = new Vector3 (0f, 0f, 90f);
+    }
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        this.transform.eulerAngles =  eulerrot;
+         eulerrot += rotationAngles * Time.deltaTime;
+    }
+} 
---
+++ Assets/quaternionrotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quaternionrotation : MonoBehaviour
+{
+
+    public Vector3 eulerRotation;
+    public Vector3 rotationAngles;
+
+    void Start()
+    {
+        eulerRotation = new Vector3 (0f, 90f, 0f);
+
+        Quaternion yRotation = Quaternion.AngleAxis (eulerRotation.y, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis (eulerRotation.x, Vector3.right);
+        Quaternion zRotation = Quaternion.AngleAxis (eulerRotation.z, Vector3.forward);
+        this.transform.rotation =  yRotation * xRotation * zRotation;
+
+        rotationAngles = new Vector3 (0f, 0f, 90f);
+    }
+
+    void Update()
+    {
+        Quaternion yRotation = Quaternion.AngleAxis (rotationAngles.y * Time.deltaTime, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis (rotationAngles.x * Time.deltaTime, Vector3.right);
+        Quaternion zRotation = Quaternion.AngleAxis (rotationAngles.z * Time.deltaTime, Vector3.forward);
+        this.transform.rotation =  yRotation * xRotation * zRotation * this.transform.rotation;
+
+       eulerRotation += rotationAngles * Time.deltaTime;
+
+    }
+
+}
